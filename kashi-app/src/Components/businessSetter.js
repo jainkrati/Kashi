7f66c955bef94d89430b0bba97ca40cd053e52c6
Krati Jain <kratijain88@gmail.com>
2024-08-11 21:16:35 +0530

    Update businessSetter.js

--- kashi-app/src/Components/businessSetter.js
+++ kashi-app/src/Components/businessSetter.js
@@ -1,39 +1,39 @@
 // component to call admin API and display the result
 
-import React, { useState, useEffect } from 'react';
-import { kashiContract } from '../DataProviders/kashiContract';
+import React, { useState, useEffect } from "react";
+import { kashiContract } from "../DataProviders/kashiContract";
 
-function BusinessSetter({
-    setter
-}) {
-    const [businessId, setBusinessId] = useState(null);
-    useEffect(() => {
-        async function fetchMaxBusinessId() {
-            const contract = kashiContract;
-            let maxBusinessId = await contract.businessId();
-            setBusinessId(maxBusinessId.toNumber());
-        }
-    
-        fetchMaxBusinessId();
-    }, []);
-    
-    return (
-        <div>
-            <label>
-            Select Business Id:
-            { businessId === null ? <p>Loading...</p> :
-            <select name="businessId" onChange={setter}>
-                {
-                Array.from({length: businessId}, (_, i) => i).map((i) => {
-                    <option value={i}>{i}</option>
-                })}
-            </select>
-            }
-            </label>
-
-        <h1>Business Id: {businessId}</h1>
-        </div>
-    );
+function BusinessSetter({ setter }) {
+  const [businessId, setBusinessId] = useState(null);
+  function handleChange(event) {
+    setter(event.target.value);
+  }
+  useEffect(() => {
+    async function fetchMaxBusinessId() {
+      const contract = kashiContract;
+      let maxBusinessId = await contract.businessId();
+      setBusinessId(maxBusinessId.toNumber() - 1);
     }
+
+    fetchMaxBusinessId();
+  }, []);
+
+  return (
+    <div>
+      <label>
+        Select Business Id:
+        {businessId === null ? (
+          <p>Loading...</p>
+        ) : (
+          <select name="businessId" onChange={handleChange}>
+            {Array.from({ length: businessId }, (_, i) => i + 1).map((i) => {
+              <option value={i}>{i}</option>;
+            })}
+          </select>
+        )}
+      </label>
+    </div>
+  );
+}
 
 export default BusinessSetter;

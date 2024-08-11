fab84b61f5dbe489e210c1d89e240a0714b5b149
Shivam Nagar <shivam.nagar@outlook.com>
2024-08-11 21:07:10 +0530

    fix

--- kashi-app/src/App.js
+++ kashi-app/src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
+import React, { useState } from 'react';
 
 // import Admin from './Components/admin';
 // import LoginComponent from './Components/login';
+import BusinessSetter from './Components/businessSetter';
 import GetBusinesses from './Components/getBusinesses';
 import GetMySubscriptions from './Components/getMySubscriptions';
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState('0x44AC194359fA44eCe6Cb2E53E8c90547BCCb95a0');
+  const [businessId, setBusinessId] = useState(1);
+
   return (
     <div className="App">
       {/* <LoginComponent /> */}
       <header className="App-header">
         {/* <Admin /> */}
-        <GetBusinesses />
-        <GetMySubscriptions />
+        <BusinessSetter setter={setBusinessId}/>
+        <GetBusinesses walletAddress={walletAddress} businessId={businessId} />
+        <GetMySubscriptions walletAddress={walletAddress} businessId={businessId} />
       </header>
     </div>
   );
